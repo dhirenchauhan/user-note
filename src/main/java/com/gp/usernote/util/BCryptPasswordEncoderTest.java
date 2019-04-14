b51f984802d06028a4973f02ccd9175529f4a73a
dhiren <dhiren@LAPTOP-BFQ1RTFM>
2019-04-14 19:33:43 +0530

    BCryptPasswordEncoder Utitlity

---
+++ src/main/java/com/gp/usernote/util/BCryptPasswordEncoderTest.java
@@ -0,0 +1,12 @@
+package com.gp.usernote.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BCryptPasswordEncoderTest {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		System.out.println(encoder.encode("testpassword"));
+	}
+
+}
